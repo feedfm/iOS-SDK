80272769436a8f3fef493dcd8eabe230f7516356
Eric Lambrecht <eric@feed.fm>
2017-09-13 11:45:56 -0700

    darn you "set -e"

--- Scripts/publishPod.sh
+++ Scripts/publishPod.sh
@@ -14,11 +14,19 @@ pod lib lint --allow-warnings
 
 # pick the release version out of the podspec
 
-VERSION=`cat ${PROJECT_DIR}/FeedMedia.podspec | perl -n -e '/s.version\s*=\s*"(.*)"/ && print $1;'`
+VERSION=`cat FeedMedia.podspec | perl -n -e '/s.version\s*=\s*"(.*)"/ && print $1;'`
+
+# confirm we haven't already deployed this version
+
+if git tag | grep "v${VERSION}\$"; then
+  echo "Code already tagged - you must create a new version"
+  exit 1
+fi
 
 # confirm the release version matches the FeedMediaCore.h #define:
 
 CHANGE=`grep "FEED_MEDIA_CLIENT_VERSION @\"${VERSION}\"" ${PROJECT_DIR}/../FeedMediaCore/Sources/FeedMediaCore.h`
+
 if [[ $CHANGE = "" ]]; then
   echo "This release version (${VERSION}) does not match FeedMediaCore.h"
   exit 1
@@ -52,17 +60,10 @@ fi
 
 cd -
 
-# create release tag
-
-TAG=$(git tag | grep "v${VERSION}\$")
-
-if [[ "$TAG" != "" ]]; then
-  echo "Code already tagged - you must create a new version"
-  exit 1
-fi
-
 git tag  -a -m "Release of version ${VERSION}" "v${VERSION}"
 
 git push
 git push --tags
+
+pod trunk push FeedMedia.podspec
 
