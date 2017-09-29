2461bf83357fe65f3237f66d7563fe2406a1df83
Eric Lambrecht <eric@feed.fm>
2017-09-29 15:27:28 -0700

    include volume adjustments to FMStationCrossfadar

--- Sources/FMStationCrossfader.h
+++ Sources/FMStationCrossfader.h
@@ -9,70 +9,96 @@
 #import <Foundation/Foundation.h>
 
 /**
- * This class keeps a list of FMStations (specified by metadata key/value pairs)
- * and time indexes. While some other media is playing (workout audio, for example),
- * this class can switch different FMStations at specified cue points.
- * If music is currently playing and the station is changed, then the music from 
- * the old station will be crossfaded with the new song in the new station.
+ * The FMStationCrossfader allows you to tie music player actions (such as station changes
+ * and volume changes) to cue points. The basic use case is to change stations and volume
+ * while playing instructional audio in a separate audio player.
  *
- * The general pattern here is to create an instance of this class with the
- * cue points you care about:
+ * Basic class usage works like so:
+ * Create an instance of the class with an array of time offsets (cue points) and the actions
+ * you want to happen at those times. The current implementation allows you to change the
+ * current station or volume. You can create an instance like so:
  *
  * FMStationCrossfader *cf = [FMSTationCrossfade stationCrossfaderWithInitialStation:@{ @"bpm": @"slow" },
- *                                               andSwitches: @[
+ *                                               andCuePoints: @[
+ *                                                      // at 5 seconds in, change the volume to 0.5
+ *                                                      5.0f, @0.5f,
+ *                                                      // also at 5 seconds in, crosfade to the station
+ *                                                      // with a 'bpm' value of 'medium'
  *                                                      5.0f, @{ @"bpm": @"medium" },
+ *                                                      // at 10 seconds in, change volume to 1.0
+ *                                                      10.0f, @1.0f,
+ *                                                      // and crossfade to station with 'bpm' of 'high'
  *                                                      10.0f, @{ @"bpm": @"high" }
  *                                               ]];
  *
- * then, just before starting your external media, call connect so this class
- * updates the global music player:
+ * Alternatively, you can create an instance of the class with no cue points and then use
+ * playStation:startingAtTime: and setVolume:startingAtTime to add cue points.
+ *
+ * When you have added all the cue points, make a call to:
  *
  * [cf connect];
  *
- * then, as your external media elapses, inform the station crossfader:
+ * Now, as your external media elapses, inform the station crossfader:
  *
  * [cf elapseToTime: _externalMediaPlayer.currentElapsedTime]
  *
- * If the user pauses playback of music then call [cf disconnect] so that music
- * stays paused until the user turns it back on again. When the user wants to
- * resume playback, call [cf reconnect].
+ * which call will cause the crossfader to update the current station and volume
+ * settings as per the cue points.
+ *
+ * When any external media that the cue points are connected to starts playback
+ * or pauses playback, you should call `[cf play]` or `[cf pause]`.
+ *
+ * If the user pauses playback of music (independent of your external media), then
+ * call [cf disconnect] so that music stays paused until the user turns it back on
+ * again. When the user wants to resume playback, call [cf reconnect].
  */
 
 @interface FMStationCrossfader : NSObject
 
 /**
  * Create a new FMStationCrossfader instance. The station passed in is considered the
- * initial station for playback, and the switches is an array of time offsets and station
- * specifiers that indicate when the station should change. 
+ * initial station for playback, and cuepoints is an array of alternating time offsets and station
+ * specifiers/volume settings that indicate when the station should change or the volume
+ * should change.
  *
  * This call:
  *
  *   FMStationCrossfader *cf = [FMSTationCrossfade stationCrossfaderWithInitialStation:@{ @"bpm": @"slow" },
- *                                               andSwitches: @[ 
+ *                                               andCuePoints: @[
+ *                                                      // change station to one with a "bpm" value of "medium"
  *                                                      5.0f, @{ @"bpm": @"medium" },
- *                                                      10.0f, @{ @"bpm": @"high" }
+ *                                                      // change station to one with a "bpm" value of "high"
+ *                                                      10.0f, @{ @"bpm": @"high" },
+ *                                                      // change volume to 0.2
+ *                                                      0.0f, @0.2f
+ *                                                      // change volume to 1.0
+ *                                                      8.0f, @1.0f
  *                                               ]];
  *
- * would make the station with 'slow' bpm active from 0 seconds in to 5 seconds in, then
+ * Would make the station with 'slow' bpm active from 0 seconds in to 5 seconds in, then
  * the station with 'medium' bpm active from 5 seconds to 10 seconds, then the station with
- * 'high' bpm active from 10 seconds to the end of the song.
+ * 'high' bpm active from 10 seconds to the end of the song. The music player volume will be set
+ * to 0.2 starting a 0 seconds in, and then changed to 1.0 at 8 seconds in.
  *
- * Setting the intial station to nil means the player starts out paused until a station switch
- * is hit.
+ * Setting the intial station to nil means the player starts out paused until a station change
+ * cuepoint is hit.
+ *
+ * CuePoints do not need to be in order, as they are ordered when the 'connect' call is
+ * made later.
  *
  * @param initialStationOptionKeysAndValues a set of key/value pairs that will be passed to
  *      FMAudioPlayer getStationWithOptionKey:Value: to get the intial station for playback
- * @param timeStationPairs an array that holds alternating NSFloats that specify time cue points and
- *       NSDictionarys that specify station searches suitable for FMAudioPlayer getStationWithOptionKey:Value:
+ * @param cuePoints an array that holds alternating NSFloats that specify time indexes and
+ *       either station queries or volume values
  * @return a new FMStationCrossfader object
  */
 
 + (FMStationCrossfader *) stationCrossfaderWithInitialStation: (NSDictionary *) initialStationOptionKeysAndValues
-                                                  andSwitches: (NSArray *) timeStationPairs;
+                                                  andCuePoints: (NSArray *) cuePoints;
 
 /**
- * Create a new FMStationCrossfader instance with a default station. Station switches can be
- * added with the playStation:startingAtItem method.
+ * Create a new FMStationCrossfader instance with a default station. Cue points can be
+ * added with the playStation:startingAtTime and setvolume:startingAtTime: methods.
  *
  * @param initialStationOptionKeysAndValues a set of key/value pairs that will be passed to
  *      FMAudioPlayer getStationWithOptionKey:Value: to get the intial station for playback
@@ -95,6 +121,19 @@
 - (void) playStation: (NSDictionary *) optionKeysAndValues startingAtTime: (float) time;
 
 /**
+ * Specify that the music player volume should be set to the given volume starting at
+ * the given time.
+ *
+ * Calls to this method must be made before calling 'connect'
+ *
+ * @param volume an NSNumber whose float value is from 0.0 to 1.0
+ * @param time the time at which the named station should become active.
+ */
+
+
+- (void) setVolume: (NSNumber *) volume startingAtTime: (float) time;
+
+/**
  * This resets the player, changes the active station to the initial station
  * and starts queueing up the first song.
  */
@@ -102,8 +141,23 @@
 - (void) connect;
 
 /**
+ * Call this to start or resume music playback rather than directly calling
+ * the global shared player instance.
+ */
+
+- (void) play;
+
+/**
+ * Call this to pause music playback rather than directly calling the global
+ * shared player instance.
+ */
+
+- (void) pause;
+
+/**
  * If the user hits pause while music is playing, then disconnect should be called to
- * prevent this class from updating playback until the user resumes music playback.
+ * prevent this class from updating playback (even if elapseTotime: is called) until
+ * the user resumes music playback.
  */
 
 - (void) disconnect;
--- Sources/FMStationCrossfader.m
+++ Sources/FMStationCrossfader.m
@@ -9,22 +9,36 @@
 #import "FMStationCrossfader.h"
 #import "FeedMedia.h"
 
-@interface FMStationAndTime : NSObject
+@interface FMCuePoint : NSObject
 
 @property (nonatomic) float time;
-@property (nonatomic, strong) FMStation *station;
 
-+ (FMStationAndTime *) station: (FMStation *) station time: (float) time;
+@property (nonatomic, strong) FMStation *station;
+@property (nonatomic, strong) NSNumber *volume;
+
++ (FMCuePoint *) station: (FMStation *) station time: (float) time;
++ (FMCuePoint *) volume: (NSNumber *) volume time: (float) time;
 
 @end
 
-@implementation FMStationAndTime
+@implementation FMCuePoint
 
-+ (FMStationAndTime *) station: (FMStation *) station time: (float) time {
-    FMStationAndTime *st = [[FMStationAndTime alloc] init];
++ (FMCuePoint *) station: (FMStation *) station time: (float) time {
+    FMCuePoint *st = [[FMCuePoint alloc] init];
     
     st.time = time;
     st.station = station;
+    st.volume = nil;
+    
+    return st;
+}
+
++ (FMCuePoint *) volume: (NSNumber *) volume time: (float) time {
+    FMCuePoint *st = [[FMCuePoint alloc] init];
+    
+    st.time = time;
+    st.station = nil;
+    st.volume = volume;
     
     return st;
 }
@@ -33,15 +47,19 @@
 
 @implementation FMStationCrossfader {
     
-    NSMutableArray *_timesAndStations;
+    NSMutableArray *_cuePoints;
     FMAudioPlayer *_player;
+    FMStation *_initialStation;
     BOOL _connected;
     
+    int _nextCuePointIndex;
+    float _lastElapsedTime;
 }
 
-- (id) init {
+- (id) initWithStation: (FMStation *) station {
     if (self = [super init]) {
-        _timesAndStations = [NSMutableArray array];
+        _initialStation = station;
+        _cuePoints = [NSMutableArray array];
         _player = [FMAudioPlayer sharedPlayer];
         _connected = NO;
     }
@@ -50,55 +68,80 @@
 }
 
 + (FMStationCrossfader *) stationCrossfaderWithInitialStation:(NSDictionary *)initialStationOptionKeysAndValues {
-    FMStationCrossfader *sc = [[FMStationCrossfader alloc] init];
-    
-    [sc playStation: initialStationOptionKeysAndValues startingAtTime: 0.0f];
+    if (initialStationOptionKeysAndValues == nil) {
+        return [[FMStationCrossfader alloc] initWithStation:nil];
 
-    return sc;
+    } else {
+        FMAudioPlayer *player = [FMAudioPlayer sharedPlayer];
+        FMStation *station = [player getStationWithOptions: initialStationOptionKeysAndValues];
+        
+        return [[FMStationCrossfader alloc] initWithStation:station];
+    }
 }
 
 + (FMStationCrossfader *) stationCrossfaderWithInitialStation: (NSDictionary *) initialStationOptionKeysAndValues
-                                                  andSwitches: (NSArray *) timeStationPairs {
+                                                  andCuePoints: (NSArray *) cuePoints {
     FMStationCrossfader *sc = [FMStationCrossfader stationCrossfaderWithInitialStation:initialStationOptionKeysAndValues];
 
-    if ((timeStationPairs.count % 2) != 0) {
-        NSLog(@"**WARNING** switches provided to FMStationCrossfader should come in pairs, but we found %lu items", (unsigned long)timeStationPairs.count);
+    if ((cuePoints.count % 2) != 0) {
+        NSLog(@"**WARNING** cuepoints provided to FMStationCrossfader should come in pairs, but we found %lu items", (unsigned long)cuePoints.count);
     }
 
-    // convert the input values into an array of FMStationAndTime objects, sorted by
-    // increasing time value
-    for (int i = 0; i < timeStationPairs.count; i += 2) {
-        NSNumber *timeObject = (NSNumber *) timeStationPairs[i];
-        NSDictionary *optionKeysAndValues = (NSDictionary *) timeStationPairs[i+1];
-        
-        [sc playStation: optionKeysAndValues startingAtTime: [timeObject floatValue]];
+    // convert the input values into an array of FMCuePoint objects
+    for (int i = 0; i < cuePoints.count; i += 2) {
+        NSNumber *timeObject = (NSNumber *) cuePoints[i];
+
+        if ([cuePoints[i+1] isKindOfClass:[NSNumber class]]) {
+            NSNumber *volume = (NSNumber *) cuePoints[i+1];
+            [sc setVolume:volume startingAtTime:[timeObject floatValue]];
+            
+        } else if ([cuePoints[i+1] isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *optionKeysAndValues = (NSDictionary *) cuePoints[i+1];
+            [sc playStation: optionKeysAndValues startingAtTime: [timeObject floatValue]];
+
+        } else {
+            NSLog(@"Unknown cue point type at time %@ is %@. Not a NSNumber or NSDictionary", timeObject, cuePoints[i+1]);
+
+        }
     }
     
     return sc;
+}
+
+- (void) setVolume:(NSNumber *)volume startingAtTime:(float)time {
+    if (_connected) {
+        NSLog(@"**WARNING** cannot append cues to crossfader once begin has been called");
+        return;
+    }
+
+    if (time < 0.0f) {
+        return;
+    }
+    
+    //NSLog(@"adding volume %f at time %f", [volume floatValue], time);
+    [_cuePoints addObject:[FMCuePoint volume:volume time:time]];
 }
 
 - (void) playStation: (NSDictionary *) optionKeysAndValues startingAtTime: (float) time {
     if (_connected) {
-        NSLog(@"**WARNING** cannot append switches to crossfader once begin has been called");
+        NSLog(@"**WARNING** cannot append cues to crossfader once begin has been called");
+        return;
+    }
+    
+    if (time < 0.0f) {
         return;
     }
     
     if (optionKeysAndValues == nil) {
-        if (time != 0.0) {
-            NSLog(@"**WARNING** only the initial station may be nil. Unable to add to station crossfader at time index %f.", time);
-
-        } else {
-            [_timesAndStations addObject:[FMStationAndTime station:nil time:time]];
-        }
-
+        NSLog(@"**WARNING** only the initial station may be nil. Unable to add to station crossfader at time index %f.", time);
         return;
     }
     
     FMStation *station = [_player getStationWithOptions: optionKeysAndValues];
     
     if (station != nil) {
-        FMLogDebug(@"adding station %@ at time %f", station.name, time);
-        [_timesAndStations addObject:[FMStationAndTime station:station time: time]];
+        //FMLogDebug(@"adding station %@ at time %f", station.name, time);
+        [_cuePoints addObject:[FMCuePoint station:station time: time]];
         
     } else {
         NSLog(@"**WARNING** unable to find station with attributes %@ for time index %f", optionKeysAndValues, time);
@@ -110,40 +153,71 @@
         return;
     }
     
-    if (_timesAndStations.count == 0) {
-        NSLog(@"*WARNING** station crossfader begin called, but no stations provided");
+    if (_cuePoints.count == 0) {
         return;
     }
 
-    // make sure all the switches are ordered properly
+    // make sure all the cue points are ordered properly
     NSSortDescriptor *sd = [NSSortDescriptor sortDescriptorWithKey:@"time" ascending:YES];
-    [_timesAndStations sortUsingDescriptors:@[ sd ]];
+    [_cuePoints sortUsingDescriptors:@[ sd ]];
     
-    // find the initial station
-    FMStation *initialStation = nil;
-    for (FMStationAndTime *st in _timesAndStations) {
-        if (st.station != nil) {
-            initialStation = st.station;
-            break;
-        }
-    }
-
-    if (initialStation == nil) {
+    NSLog(@"there are %ld cue points", (long) _cuePoints.count);
+    
+    _nextCuePointIndex = 0;
+    _lastElapsedTime = -1.0f;
+    
+    if (_initialStation == nil) {
         // no station specified anywhere.. so just reset the player
         [_player stop];
 
-    } else if ([initialStation isEqual:_player.activeStation]) {
+    } else if ([_initialStation isEqual:_player.activeStation]) {
         // already in the station - make sure we're paused
         [_player pause];
         
     } else {
         // tune to the initial station
         [_player stop];
-        _player.activeStation = initialStation;
+        _player.activeStation = _initialStation;
         
     }
 
     _connected = YES;
+}
+
+- (void) play {
+    if (!_connected || (_player.playbackState == FMAudioPlayerPlaybackStateUnavailable)) {
+        return;
+    }
+    
+    if (_initialStation != nil) {
+        [_player play];
+        return;
+    }
+    
+    if (_lastElapsedTime == -1.0f) {
+        // no initial station, and we haven't elapsed playback yet, so don't start anything
+        return;
+    }
+
+    // start playback if we've tuned to a station by now
+    for (int x = _nextCuePointIndex; x >= 0; x--) {
+        FMCuePoint *cp = (FMCuePoint *) _cuePoints[x];
+        
+        if (cp.station != nil) {
+            [_player play];
+            return;
+        }
+    }
+    
+    // otherwise, don't start playback just yet
+}
+
+- (void) pause {
+    if (!_connected || (_player.playbackState == FMAudioPlayerPlaybackStateUnavailable)) {
+        return;
+    }
+
+    [_player pause];
 }
 
 - (void) disconnect {
@@ -155,35 +229,59 @@
 }
 
 - (void) elapseToTime: (float) time {
-    if (!_connected || (_timesAndStations.count == 0)) {
+    if (!_connected || (_cuePoints.count == 0)) {
         return;
     }
     
-    FMStation *station = ((FMStationAndTime *) _timesAndStations[0]).station;
+    //NSLog(@"advancing to time %f. ncpi is %d", time, _nextCuePointIndex);
     
-    for (FMStationAndTime *st in _timesAndStations) {
-        if (time < st.time) {
-            break;
-        }
-        
-        station = st.station;
+    // user jumped back in time for some reason, so reset things
+    if (_lastElapsedTime > time) {
+        _lastElapsedTime = -1.0f;
+        _nextCuePointIndex = 0;
     }
     
-    if (station == nil) {
-        if ((_player.playbackState != FMAudioPlayerPlaybackStatePaused) ||
-            (_player.playbackState != FMAudioPlayerPlaybackStateReadyToPlay)) {
-            [_player pause];
+    FMStation *station = nil;
+    NSNumber *volume = nil;
+    
+    FMCuePoint *cp = (_cuePoints.count > _nextCuePointIndex) ? _cuePoints[_nextCuePointIndex] : nil;
+    while ((cp != nil) && (cp.time <= time)) {
+        if (cp.station != nil) {
+            station = cp.station;
         }
-
-    } else {
+        
+        if (cp.volume != nil) {
+            volume = cp.volume;
+        }
+        
+        _nextCuePointIndex++;
+        cp = (_cuePoints.count > _nextCuePointIndex) ? _cuePoints[_nextCuePointIndex] : nil;
+    }
+    
+    if (station != nil) {
         if (![_player.activeStation isEqual:station]) {
+            //NSLog(@"Changing station to %@", station);
+            
             [_player setActiveStation:station withCrossfade:YES];
         }
+
+        [_player play];
+
+    } else if ((_lastElapsedTime == -1.0f) && (_initialStation != nil)) {
+        // if this is the first call to elapseToTime and we have an initial
+        // station, then kick off playback of that station
+        //NSLog(@"Kicking off initial playback of station %@", station);
         
-        if (_player.playbackState != FMAudioPlayerPlaybackStatePlaying) {
-            [_player play];
-        }
+        [_player play];
     }
+    
+    if ((volume != nil) && (_player.mixVolume != [volume floatValue])) {
+        //NSLog(@"Changing volume to %f", [volume floatValue]);
+        
+        _player.mixVolume = [volume floatValue];
+    }
+    
+    _lastElapsedTime = time;
 }
 
 @end
