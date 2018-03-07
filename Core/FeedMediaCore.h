c91d45aeaa5f6fde88b197cbfa7b6d3c160e23c3
Eric Lambrecht <eric@feed.fm>
2018-03-06 19:02:11 -0800

    version bump

--- Core/FeedMediaCore.h
+++ Core/FeedMediaCore.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Feed Media. All rights reserved.
 //
 
-#define FEED_MEDIA_CLIENT_VERSION @"4.0.26"
+#define FEED_MEDIA_CLIENT_VERSION @"4.0.27"
 
 // All public headers
 #import "FMAudioItem.h"
