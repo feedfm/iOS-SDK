4dc04b41bf9cef2f7980e0feb9f66aac167c4834
Arveen <arveen@feed.fm>
2019-03-20 12:50:00 -0700

    Update to version 4.2.4

--- Core/FeedMediaCore.h
+++ Core/FeedMediaCore.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Feed Media. All rights reserved.
 //
 
-#define FEED_MEDIA_CLIENT_VERSION @"4.2.4-beta"
+#define FEED_MEDIA_CLIENT_VERSION @"4.2.4"
 
 // All public headers
 #import "FMAudioItem.h"
