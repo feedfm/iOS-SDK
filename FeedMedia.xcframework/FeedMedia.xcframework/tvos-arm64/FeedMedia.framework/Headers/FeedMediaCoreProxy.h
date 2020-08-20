fab17e01f04e808028c281b44d2d5b9626cec54c
Arveen <arveen@feed.fm>
2020-08-20 17:57:09 +0100

    Update for release of version 4.4.3-beta2

---
+++ FeedMedia.xcframework/FeedMedia.xcframework/tvos-arm64/FeedMedia.framework/Headers/FeedMediaCoreProxy.h
@@ -0,0 +1,18 @@
+//
+//  FeedMediaCoreProxy.h
+//  FeedMedia
+//
+//  In the CocoaPod library, the Core stuff is not a separate
+//  module, so the #includes are different. This file picks
+//  the proper #include
+//
+//  Created by Eric Lambrecht on 9/11/17.
+//  Copyright © 2017 Feed Media. All rights reserved.
+//
+
+#ifndef FeedMediaCoreProxy_h
+#define FeedMediaCoreProxy_h
+
+#include "FeedMediaCore.h"
+
+#endif /* FeedMediaCoreProxy_h */
